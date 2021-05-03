f4fd2d17a0490f8da2b646b15a2bb4f9f14a42c4
W-L <weilguny.lukas@gmail.com>
2021-05-03 19:45:34 +0100

    add the scripts that run ddccontrol

---
+++ dim.sh
@@ -0,0 +1,3 @@
+ #! /bin/bash
+ddccontrol -r 0x12 -w 50 dev:/dev/i2c-3
+ddccontrol -r 0x10 -w 50 dev:/dev/i2c-3
---
+++ vdim.sh
@@ -0,0 +1,3 @@
+ #! /bin/bash
+ddccontrol -r 0x12 -w 30 dev:/dev/i2c-3
+ddccontrol -r 0x10 -w 30 dev:/dev/i2c-3
